cf5b3ad9514801c9b876006e34f9e0c2af90aaab
tpluscode <tpluscode@users.noreply.github.com>
2020-05-16 21:33:10 +0200

    feat: add type declarations

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+import { Sink } from 'rdf-js';
+import { EventEmitter } from 'events';
+
+export interface SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, Sink<InputStream, OutputStream>> {
+    import(mediaType: string, input: InputStream, options?: any): OutputStream | null;
+}
+
+export class SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, Sink<InputStream, OutputStream>> implements SinkMap<InputStream, OutputStream> {
+}
