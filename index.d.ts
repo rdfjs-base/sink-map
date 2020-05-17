47547b5d70cb1436339650da3b9996d56bfd038d
tpluscode <tpluscode@users.noreply.github.com>
2020-05-17 10:42:26 +0200

    chore: update to accurate types

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,13 @@
 import { Sink } from 'rdf-js';
 import { EventEmitter } from 'events';
 
-export interface SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, Sink<InputStream, OutputStream>> {
+type SinkOrLazy<InputStream extends EventEmitter, OutputStream extends EventEmitter> =
+    Sink<InputStream, OutputStream> |
+    (() => Sink<InputStream, OutputStream>)
+
+export interface SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, SinkOrLazy<InputStream, OutputStream>> {
     import(mediaType: string, input: InputStream, options?: any): OutputStream | null;
 }
 
-export class SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, Sink<InputStream, OutputStream>> implements SinkMap<InputStream, OutputStream> {
+export class SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, SinkOrLazy<InputStream, OutputStream>> implements SinkMap<InputStream, OutputStream> {
 }
