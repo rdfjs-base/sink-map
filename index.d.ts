c9a28eba4005ef01798137d745a8b4a60bfcecf3
tpluscode <tpluscode@users.noreply.github.com>
2020-06-16 18:13:38 +0200

    fix: lazy sink should also allow promise

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,12 @@
 import { Sink } from 'rdf-js';
 import { EventEmitter } from 'events';
 
+type LazySink<InputStream extends EventEmitter, OutputStream extends EventEmitter> =
+    () => (Sink<InputStream, OutputStream> | Promise<Sink<InputStream, OutputStream>>)
+
 type SinkOrLazy<InputStream extends EventEmitter, OutputStream extends EventEmitter> =
     Sink<InputStream, OutputStream> |
-    (() => Sink<InputStream, OutputStream>)
+    LazySink<InputStream, OutputStream>
 
 export interface SinkMap<InputStream extends EventEmitter, OutputStream extends EventEmitter> extends Map<string, SinkOrLazy<InputStream, OutputStream>> {
     import(mediaType: string, input: InputStream, options?: any): OutputStream | null;
